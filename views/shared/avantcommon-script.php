a0b6c1a0429c6888721661faa281f2923f039d07
gsoules <gsoules@avantlogic.com>
2018-04-12 19:31:05 -0400

    Changes to support Lightbox.

---
+++ views/shared/avantcommon-script.php
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+jQuery(document).ready(function()
+{
+    jQuery('.lightbox').magnificPopup(
+        {
+            type: 'image',
+            gallery:{
+                enabled:true
+            }
+        }
+    );
+});
+</script>
