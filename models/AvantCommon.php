8b1926272b7888b7ba1a29046378462aa070669c
gsoules <gsoules@avantlogic.com>
2018-05-05 11:36:50 -0400

    Rewrite Post getter/setter logic.

--- models/AvantCommon.php
+++ models/AvantCommon.php
@@ -4,18 +4,48 @@ class AvantCommon
 {
     public static function elementHasPostedValue($elementId)
     {
-        return !empty($_POST['Elements'][$elementId][0]['text']);
+        // Get the values from all of this element's input fields. Return true if any have a value.
+        $values = $_POST['Elements'][$elementId];
+
+        foreach ($values as $value)
+        {
+            if (!empty($value['text']))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public static function getPostTextForElementId($elementId)
+    public static function getPostedValues($elementId)
     {
-        return $_POST['Elements'][$elementId][0]['text'];
+        $texts = array();
+
+        if (!isset($_POST['Elements'][$elementId]))
+        {
+            $texts = array('');
+        }
+        else
+        {
+            $values = $_POST['Elements'][$elementId];
+
+            foreach ($values as $value)
+            {
+                $texts[] = $value['text'];
+            }
+        }
+
+        return $texts;
     }
 
     public static function getPostTextForElementName($elementName)
     {
+        // Return the element's posted value. If it has more than one, only return the first.
+        // Use current() instead of [0] in case the 0th element was deleted using the Remove button.
         $elementId = ItemMetadata::getElementIdForElementName($elementName);
-        return self::getPostTextForElementId($elementId);
+        $values = $_POST['Elements'][$elementId];
+        $text = current($values)['text'];
+        return $text;
     }
 
     public static function setPostTextForElementId($elementId, $text)
