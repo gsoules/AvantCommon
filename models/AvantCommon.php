fa77121531c1926bf810fc44102d83973e99cdaf
George Soules <gsoules@avantlogic.com>
2018-09-11 08:20:31 -0400

    Fix bug where missing required elements were not flagged for a new item.

--- models/AvantCommon.php
+++ models/AvantCommon.php
@@ -70,6 +70,6 @@ class AvantCommon
         // where an item is saved as part of another operation such as a batch edit or a reindex of search records.
         // When the user clicks the Save Changes button, the page posts to submit the Edit form to the server. The
         // other cases are usually peformed as part of a server-side background job that operates on multiple items.
-        return isset($_POST['submit']) && $_POST['submit'] == 'Save Changes';
+        return isset($_POST['submit']) && ($_POST['submit'] == 'Save Changes' || $_POST['submit'] == 'Add Item');
     }
 }
