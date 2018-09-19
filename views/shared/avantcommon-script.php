7bdfb9ca7484eec8ce41154a58f24c8499b5b706
George Soules <gsoules@avantlogic.com>
2018-09-19 09:01:06 -0400

    Show white caption area below lightbox image.

--- views/shared/avantcommon-script.php
+++ views/shared/avantcommon-script.php
@@ -14,16 +14,16 @@ jQuery(document).ready(function()
                     var href = item.el.attr('href');
                     var hasItemLink = id.length > 0;
                     var fileName = hasItemLink ? '' : title;
-                    var imageLink = '<a class="lightbox-image-link" href="' + href + '" target="_blank" title="View image in a separate window"></a>' + fileName;
+                    var imageLink = '<div><a class="lightbox-image-link" href="' + href + '" target="_blank" title="View image in a separate window"></a>' + fileName + '</div>';
                     var itemLink = '';
                     if (hasItemLink)
                     {
                         var itemPath = '<?php echo $path; ?>'  + id;
-                        itemLink = '<a class="lightbox-link" href="' + itemPath + '" title="View item">' + title + '</a>';
+                        itemLink = '<div>' + title + '</div>';
+                        itemLink += '<div><a class="lightbox-link" href="' + itemPath + '" title="View item">View this item</a></div>';
                     }
                     caption = imageLink + itemLink;
-                    return caption;
-                }
+                    return caption;               }
             }
         }
     );
