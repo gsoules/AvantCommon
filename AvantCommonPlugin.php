443aba24ac8c532ff199a1ab40c5cd8385e7518d
gsoules <gsoules@avantlogic.com>
2020-02-28 16:53:49 -0500

    Emit js.cookie.js since it's used by other plugins dependent on AvantCommon.

--- AvantCommonPlugin.php
+++ AvantCommonPlugin.php
@@ -94,6 +94,7 @@ class AvantCommonPlugin extends Omeka_Plugin_AbstractPlugin
     {
         queue_css_file('magnific-popup');
         queue_js_file('jquery.magnific-popup.min');
+        queue_js_file('js.cookie');
         queue_js_file('avantcommon-script');
 
         queue_css_file('avantcommon');
