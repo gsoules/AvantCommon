ba73fc8d9299afdf0a39fd2f3487c3a08ac4390e
gsoules <gsoules@avantlogic.com>
2020-09-14 17:42:25 -0400

    Force refresh of avantcommon.css.

--- AvantCommonPlugin.php
+++ AvantCommonPlugin.php
@@ -97,7 +97,8 @@ class AvantCommonPlugin extends Omeka_Plugin_AbstractPlugin
         queue_js_file('js.cookie-2.2.1.min');
         queue_js_file('avantcommon-script');
 
-        queue_css_file('avantcommon');
+        $version = OMEKA_VERSION . '.1';
+        queue_css_file('avantcommon', 'all', false, 'css', $version);
     }
 
     public function hookAdminHead($args)
